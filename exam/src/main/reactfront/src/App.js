53463563b771a3eacd51e2d09babcc7d6ee91c67
hyewonny2327 <wonny2327@gmail.com>
2023-11-28 02:28:03 +0900

    :bug: Npm start 실행 오류 해결

--- exam/src/main/reactfront/src/App.js
+++ exam/src/main/reactfront/src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import anime from 'animejs';
+import Shapes from './pages/MainPage';
+import styled from 'styled-components';
+import AvatarPage from './pages/AvatarPage';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import AvatarPage_2 from './pages/AvatarPage_2'
+import Result from './pages/Result';
+import MainPage from './pages/MainPage';
+import LoadingPage from './pages/LoadingPage';
+const GlobalStyle = styled.div`
+cursor: url('/img/cursor/defaultCursor.png') 2 2, auto;
+overflow:hidden;
+`
+  function App() {
+    return (
+      <GlobalStyle className="App">
+        <header className="App-header">
+          <Routes>
+            <Route path="/" element={<MainPage />} />
+            <Route path="avatar/0" element={<AvatarPage />}/>
+            <Route path="avatar/*" element={<AvatarPage_2 />} />
+            <Route path="avatar/loading" element={<LoadingPage />} />
+            <Route path="avatar/result" element={<Result/>} />
+          
+          </Routes>
+        </header>
+      </GlobalStyle>
+    );
+  }
+  
 
 export default App;
