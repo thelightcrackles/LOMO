7f2e39e8add009ea8b6f039b29d4bca8222e7e7d
hyewonny2327 <144579568+hyewonny2327@users.noreply.github.com>
2023-11-28 02:36:34 +0900

    Delete src directory
    중복되는 폴더 제거

--- src/pages/GlobalStyle.js
+++
@@ -1,7 +0,0 @@
-import { createGlobalStyle } from 'styled-components';
-
-const GlobalStyle = createGlobalStyle`
-cursor: url('/img/cursor/defaultCursor.png') 2 2, auto;
-`;
-
-export default GlobalStyle;
